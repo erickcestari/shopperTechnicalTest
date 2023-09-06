78e24541f4423e9d6e7ff19965b22ef82a8c4e87
Erick Cestari <erickcestari03@gmail.com>
2023-09-05 23:03:47 -0300

    adding prisma

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const app = express()
+
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`)
 })
