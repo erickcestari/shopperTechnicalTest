ea32e93ad0b9add5b68c871b3a3cd1136ff5fe61
Erick Cestari <erickcestari03@gmail.com>
2023-09-07 20:24:22 -0300

    clening ode

--- front/src/components/displayData.tsx
+++ front/src/components/displayData.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { Paper } from '@mui/material'
 
-interface displaytDataProps  {
+interface displaytDataProps {
   dataValidated: {
     product: {
       code: string
@@ -14,24 +15,24 @@ interface displaytDataProps  {
 
 export const DisplayData = (props: displaytDataProps) => {
   return (
-    <div className="flex flex-col">
-    <h1 className="text-2xl text-zinc-100">Dados validados:</h1>
-    <hr />
-    <div className="flex flex-col">
-      {props.dataValidated.product && (
-        <>
-          <span className="text-zinc-100">ID: {props.dataValidated.product.code}</span>
-          <span className="text-zinc-100">Nome: {props.dataValidated.product.name}</span>
-          <span className="text-zinc-100">Preço atual: {props.dataValidated.product.sales_price}</span>
-          <span className="text-zinc-100">Novo preço: {props.dataValidated.new_price}</span>
-        </>
-      )}
-      {props.dataValidated.error ? (
-        <span className="text-red-500">Erro: {props.dataValidated.error}</span>
-      ) : (
-        <span className="text-green-500">Sucesso</span>
-      )}
-    </div>
-  </div>
+    <Paper className="flex flex-col bg-zinc-800 p-5 my-5">
+      <h1 className="text-2xl text-zinc-100">Dados validados:</h1>
+      <hr />
+      <div className="flex flex-col">
+        {props.dataValidated.product && (
+          <>
+            <span className="text-zinc-100">ID: {props.dataValidated.product.code}</span>
+            <span className="text-zinc-100">Nome: {props.dataValidated.product.name}</span>
+            <span className="text-zinc-100">Preço atual: {props.dataValidated.product.sales_price}</span>
+            <span className="text-zinc-100">Novo preço: {props.dataValidated.new_price}</span>
+          </>
+        )}
+        {props.dataValidated.error ? (
+          <span className="text-red-500">Erro: {props.dataValidated.error}</span>
+        ) : (
+          <span className="text-green-500">Sucesso</span>
+        )}
+      </div>
+    </Paper>
   )
 }
